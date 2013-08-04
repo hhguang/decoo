78f7402b417a2a0adac16a08df403a1b2e8370fa
varro <hhguang@gmail.com>
2013-08-04 15:54:07 +0800

    porder fix

--- app/models/porder.rb
+++ app/models/porder.rb
@@ -1,5 +1,5 @@
 class Porder < ActiveRecord::Base
-  attr_accessible :name, :outstock_id, :quantity, :toy_id, :user_id,:is_out,:porder_items_attributes
+  attr_accessible :name, :outstock_id, :quantity, :toy_id, :user_id,:is_out,:porder_items_attributes,:memo
 
   belongs_to :toy
   belongs_to :user
