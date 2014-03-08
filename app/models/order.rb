76f31b4f550328cb55574be925e7ca593e4fe8a0
hhguang <hhguang@gmail.com>
2014-03-08 17:57:02 +0800

    order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,5 @@ class Order < ActiveRecord::Base
   has_many :order_items
   has_many :order_stocks
 
-  accepts_nested_attributes_for :order_items,:reject_if => lambda { |a| a[:bh].blank? || a[:quantity].blank?  }
+  accepts_nested_attributes_for :order_items,:reject_if => lambda { |a| a[:product_bh].blank? || a[:quantity].blank?  }, :allow_destroy => true
 end
